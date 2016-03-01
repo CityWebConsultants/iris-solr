40ab4097cd2d65b3d2a61bbd34b7f9041813087e
facascante <chito.cascante@gmail.com>
2016-03-01 23:41:16 +0800

    [#67] refactor connection handler to be used for query builder

--- irisSolr.js
+++ irisSolr.js
@@ -17,6 +17,10 @@ var search = {
 iris.route.get('/search', search.query, function (req, res) {
 
 });
+
+/**
+ * Endpoint to manage Solr connection details.
+ */
 iris.route.get('/admin/config/search/solr', search.config, function (req, res) {
   iris.modules.frontend.globals.parseTemplateFile(["admin-solr"], ['html'], {
     'current': req.irisRoute.options,
@@ -35,7 +39,6 @@ iris.route.get('/admin/config/search/solr', search.config, function (req, res) {
 
 /**
  * Defines form adminSolr.
- * Input coneection details for Mysql database
  */
 iris.modules.irisSolr.registerHook("hook_form_render_adminSolr", 0, function (thisHook, data) {
 
@@ -44,7 +47,7 @@ iris.modules.irisSolr.registerHook("hook_form_render_adminSolr", 0, function (th
     iris.modules.irisSolr.globals.renderAdminSolrForm(thisHook, data, config);
 
   }, function (fail) {
-    console.log("fail", fail);
+
     iris.modules.irisSolr.globals.renderAdminSolrForm(thisHook, data, false);
 
   });
@@ -52,26 +55,28 @@ iris.modules.irisSolr.registerHook("hook_form_render_adminSolr", 0, function (th
 });
 
 iris.modules.irisSolr.globals.renderAdminSolrForm = function (thisHook, data, config) {
-  console.log(data);
-  console.log(config);
+
   data.schema.host = {
     "type": "text",
     "title": "Host",
     "required": true,
     "default": config.host ? config.host : ""
   };
+
   data.schema.port = {
     "type": "text",
     "title": "Port",
     "required": true,
     "default": config.port ? config.port : ""
   };
+
   data.schema.core = {
     "type": "text",
     "title": "Core",
     "required": true,
     "default": config.core ? config.core : ""
   };
+
   data.schema.path = {
     "type": "text",
     "title": "Path",
@@ -81,6 +86,128 @@ iris.modules.irisSolr.globals.renderAdminSolrForm = function (thisHook, data, co
 
   thisHook.finish(true, data);
 }
+
+
+
+/**
+ * Given a generated SQL query, create a Solr connection and execute the query.
+ */
+iris.modules.irisSolr.globals.executeQuery = function (action, content, callback) {
+
+  if (action !== false) {
+
+    try {
+
+      var connection = iris.modules.irisSolr.globals.getSolrConnection(null);
+      if (connection) {
+        connection[action](content, function (err, obj) {
+
+          if (err) {
+
+            iris.log("error", "Error on query execution. Please check your query details" + JSON.stringify(err));
+            callback(false);
+            return false;
+
+          } else {
+
+            connection.commit({ waitSearcher: false }, function (err, resp) {
+
+              callback(obj);
+              return false;
+
+            });
+
+          }
+
+        });
+      }
+      else {
+        callback(false);
+        return false;
+      }
+
+    } catch (e) {
+
+      iris.log("error", "Error connecting to Solr server. Please check your connection details.", e);
+
+      iris.message("Error connecting to Solr server. Please check your connection details.", "error");
+
+      callback(false);
+      return false;
+
+
+    }
+  } else {
+
+    callback(false);
+    return false;
+  }
+}
+
+/**
+ * Helper function to generate search result based on filter.
+ */
+iris.modules.irisSolr.globals.generateSearch = function (req, res, template, controller) {
+
+};
+
+/**
+ * Helper function to generate filter base from url.
+ */
+iris.modules.irisSolr.globals.generateQuery = function (content) {
+
+  var connection = iris.modules.irisSolr.globals.getSolrConnection(null);
+  if (connection) {
+    return connection.createQuery()
+      .q(content.filter)
+      .start(content.start || 0)
+      .rows(content.rows || 10)
+  }
+  else{
+    return false;
+  }
+
+};
+
+/**
+ * Initialize the connection object.
+ */
+iris.modules.irisSolr.globals.getSolrConnection = function (config) {
+
+  config = config || iris.readConfigSync('irisSolr', 'adminSolr');
+  if (config) {
+    try {
+
+      var solr = require('solr-client');
+
+      return solr.createClient({
+        host: config.host,
+        port: config.port,
+        core: config.core,
+        path: config.path
+      });
+
+    } catch (e) {
+
+      iris.log("error", "Error connecting to Solr server. Please check your connection details.", e);
+
+      iris.message("Error connecting to Solr server. Please check your connection details.", "error");
+
+      return false;
+
+
+    }
+  }
+  else {
+
+    iris.log("error", "Error reading configuration file. irisSolr : adminSolr" + fail);
+
+    iris.message("Error reading configuration file. irisSolr : adminSolr", "error");
+
+    return false;
+
+  }
+};
 
 /**
  * Submit handler for adminSolr.
@@ -111,96 +238,29 @@ iris.modules.irisSolr.registerHook("hook_form_submit_adminSolr", 0, function (th
 });
 
 /**
- * Given a generated SQL query, create a Solr connection and execute the query.
+ * Create record handler for Solr.
  */
-iris.modules.irisSolr.globals.executeQuery = function (action, content, callback) {
-  console.log("chito", action);
-  if (action !== false) {
-
-    iris.readConfig('irisSolr', 'adminSolr').then(function (config) {
-
-      try {
-
-        var connection = iris.modules.irisSolr.globals.getSolrConnection(config);
-        connection[action](content, function (err, obj) {
-          if (err) {
-            console.log("error", "2Error connecting to Solr server. Please check your connection details" + JSON.stringify(err));
-            callback(false);
-            return false;
-          } else {
-            connection.commit({ waitSearcher: false }, function (err, resp) {
-              console.log(resp);
-              callback(obj);
-              return false;
-            });
-            
-            
-            
-          }
-        });
-
-      } catch (e) {
-
-        console.log("error", "3Error connecting to Solr server. Please check your connection details", e);
-
-        iris.message("Error connecting to Solr server. Please check your connection details.", "error");
-
-        callback(false);
-        return false;
-
-
-      }
-    }, function (fail) {
-
-      console.log("error", "Error connecting to Solr server. Please check your connection details" + fail);
-
-      iris.message("Error connecting to Solr server. Please check your connection details", "error");
-
-      callback(false);
-      return false;
-
-
-    });
-  } else {
-
-    callback(false);
-    return false;
-  }
-}
-
-/**
- * Initialize the connection object.
- * TODO: Create an admin form to enter DB details instead of hard-coding them.
- */
-iris.modules.irisSolr.globals.getSolrConnection = function (config) {
-
-  var solr = require('solr-client');
-
-  var client = solr.createClient({
-    host: config.host,
-    port: config.port,
-    core: config.core,
-    path: config.path
-  });
-
-  return client;
-};
-
 iris.modules.irisSolr.registerHook("hook_entity_create", 1, function (thisHook, data) {
-  console.log(data);
+
   data.id = data._id
+
   iris.modules.irisSolr.globals.executeQuery("add", data, function (resp) {
-    console.log(data);
+
     thisHook.finish(true, data);
 
   });
 
 });
 
+/**
+ * Update record handler for Solr.
+ */
 iris.modules.irisSolr.registerHook("hook_entity_updated", 1, function (thisHook, data) {
 
   iris.modules.irisSolr.globals.executeQuery("deleteByQuery", 'id:' + data.eid, function (resp) {
+
     data.id = data._id
+
     iris.modules.irisSolr.globals.executeQuery("add", data, function (resp) {
 
       thisHook.finish(true, data);
@@ -210,8 +270,11 @@ iris.modules.irisSolr.registerHook("hook_entity_updated", 1, function (thisHook,
   });
 });
 
+/**
+ * Delete record handler for Solr.
+ */
 iris.modules.irisSolr.registerHook("hook_entity_delete", 1, function (thisHook, data) {
-  console.log(data);
+
   iris.modules.irisSolr.globals.executeQuery("deleteByQuery", 'id:' + data.eid, function (resp) {
 
     thisHook.finish(true, data);
